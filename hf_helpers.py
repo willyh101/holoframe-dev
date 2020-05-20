6aa2641333b4d432a8bd0a01534709dca4535c04
Will <hendricksw@gmail.com>
2020-05-19 18:13:06 -0700

    fixed initial files

---
+++ hf_helpers.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+
+def _get_hf_group_unstacked(df, cols, on, times, inplace):
+    if times is not None:
+        if len(times==4):
+            # 4 numbers, so subtract the baseline response
+            bdf = df[(df.time > times[0]) & (df.time < times[1])]
+            rdf = df[(df.time > times[2]) & (df.time < times[3])]
+            grp_df = rdf.groupby(cols)[on].mean().unstack() - bdf.groupby(cols)[on].mean().unstack()
+        elif len(times==2):
+            # if only 2 numbers, don't baseline
+            df = df[(df.time > times[0]) & (df.time < times[1])]
+        else: # else it's wrong
+            raise IndexError(f'Length of times should either be None (whole trace), 2 (no baselining) or 4 (baselining), not {len(times)}.')
+    else: # if you want the whole trace
+        grp_df = df.groupby(cols)[on].mean().unstack()
+
+    if not inplace: # returns a copy
+        return grp_df.copy()
+    else: # returns a view, caution with editing this dataframe!
+        print('warning! not returning a copy... expect weird results.')
+        return grp_df
+
+def _handle_xwise_data(vals, name):
+    """
+    For handling inputs that append to a HoloFrame. This verifies the input
+    and converts them into pandas structures for appending.
+    """
+    if isinstance(vals, (pd.Series, pd.DataFrame)):
+        # if a series or dataframe, no need to do anything
+        appendthis = vals
+    elif isinstance(vals, (np.ndarray, list)):
+        # for passing array-likes, must be 1 dim
+        if isinstance(vals, np.ndarray):
+            assert hasattr(vals, 'ndim') and vals.ndim == 1, 'Must pass array with one dimension only.'
+        # need to have a name to name the col
+        if name is not None:
+            appendthis = pd.Series(vals, name=name)
+        else:
+            raise ValueError('Name is required for lists and arrays.')
+    elif isinstance(vals, dict):
+        # for passing dicts, make them a dataframe
+        appendthis = pd.DataFrame(vals)
+    else:
+        raise TypeError('Must pass list, dict, np.ndarray, pd.Series, or pd.DataFrame only.')
+
+    return appendthis
+
+def _add_xwise(appendthis, on, replace):
+    try:
+        # join on trials
+        frame = frame.join(appendthis, on=on)
+    except ValueError:
+        # if col already exists, pandas throws an error
+        if replace:
+            # drop the cols, method depends on whether joining a df or series
+            if isinstance(appendthis, pd.DataFrame):
+                frame = frame.drop(columns=appendthis.columns).join(appendthis, on=on)
+            else:
+                frame = frame.drop(columns=appendthis.name).join(appendthis, on=on)
+        else:
+            raise ValueError(f'Trying to append but column(s) already exist. Use replace = True to override.')
+
+    return frame
--- holoframe_main.py
+++ holoframe_main.py
@@ -3,28 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import xarray as xr
-
-def _get_hf_group_unstacked(df, cols, on, times, inplace):
-    if times is not None:
-        if len(times==4):
-            # 4 numbers, so subtract the baseline response
-            bdf = df[(df.time > times[0]) & (df.time < times[1])]
-            rdf = df[(df.time > times[2]) & (df.time < times[3])]
-            grp_df = rdf.groupby(cols)[on].mean().unstack() - bdf.groupby(cols)[on].mean().unstack()
-        elif len(times==2):
-            # if only 2 numbers, don't baseline
-            df = df[(df.time > times[0]) & (df.time < times[1])]
-        else: # else it's wrong
-            raise IndexError(f'Length of times should either be None (whole trace), 2 (no baselining) or 4 (baselining), not {len(times)}.')
-    else: # if you want the whole trace
-        grp_df = df.groupby(cols)[on].mean().unstack()
-
-    if not inplace: # returns a copy
-        return grp_df.copy()
-    else: # returns a view, caution with editing this dataframe!
-        print('warning! not returning a copy... expect weird results.')
-        return grp_df
-
+from hf_helpers import _get_hf_group_unstacked, _handle_xwise_data, _add_xwise
 
 class HoloFrame(pd.DataFrame):
 ###---warning! don't touch this section. chnages will break it.---###
@@ -57,46 +36,42 @@ class HoloFrame(pd.DataFrame):
         else:
             frame = self.copy()
 
-        if isinstance(vals, (pd.Series, pd.DataFrame)):
-            # if a series or dataframe, no need to do anything
-            appendthis = vals
-        elif isinstance(vals, (np.ndarray, list)):
-            # for passing array-likes, must be 1 dim
-            if isinstance(vals, np.ndarray):
-                assert hasattr(vals, 'ndim') and vals.ndim == 1, 'Must pass array with one dimension only.'
-            # need to have a name to name the col
-            if name is not None:
-                appendthis = pd.Series(vals, name=name)
-            else:
-                raise ValueError('Name is required for lists and arrays.')
-        elif isinstance(vals, dict):
-            # for passing dicts, make them a dataframe
-            appendthis = pd.DataFrame(vals)
-        else:
-            raise TypeError('Must pass list, dict, np.ndarray, pd.Series, or pd.DataFrame only.')
+        appendthis = _handle_xwise_data(vals) # verify/handle the input data
 
+        # whatever you are trying to append should be the same length as the target
         assert frame.trial.nunique() == appendthis.size, 'Number of trials and length of trialwise data must match.'
 
-        try:
-            # join on trials
-            frame = frame.join(appendthis, on='trial')
-        except ValueError:
-            # if col already exists, pandas throws an error
-            if replace:
-                # drop the cols, method depends on whether joining a df or series
-                if isinstance(appendthis, pd.DataFrame):
-                    frame = frame.drop(columns=appendthis.columns).join(appendthis, on='trial')
-                else:
-                    frame = frame.drop(columns=appendthis.name).join(appendthis, on='trial')
-            else:
-                raise ValueError(f'Trying to append but column(s) already exist. Use replace=True to override.')
+        frame = _add_xwise(appendthis, 'trial', replace)
+
+        if not inplace:
+            return frame
+
+    def add_cellwise(self, vals, name=None, replace=False, inplace=False):
+        if inplace:
+            frame = self
+        else:
+            frame = self.copy()
+
+        appendthis = _handle_xwise_data(vals, name) # verify/handle the input data
+
+        assert frame.cell.nunique() == appendthis.size, 'Number of cells and length of cellwise data must match.'
+
+        frame = _add_xwise(appendthis, 'cell', replace)
 
         if not inplace:
             return frame
 
 
-    # def times(self, frame_rate):
-    #     self['secs'] = self['time']/frame_rate
+    def add_secs(self, frame_rate, inplace):
+        if inplace:
+            frame = self
+        else:
+            frame = self.copy()
+
+        self['secs'] = self['time']/frame_rate
+
+        if not inplace:
+            return frame
 
 
     def meanby(self, conds, on='df', times=None, inplace=False):
@@ -114,9 +89,14 @@ class HoloFrame(pd.DataFrame):
 
 
     @classmethod
-    def from_traces(cls, traces, trialwise_data=None):
+    def from_traces(cls, traces, trialwise_data=None, frame_rate=None):
         """
         Takes a cells x trials x time array and morphs it into a HoloFrame.
+        Can add any trialwise data during construction.
+
+        Inputs:
+            traces (arr): cell x trial x time array of experiment
+            trialwise_data (arr): vector of any trialwise data to append to df
         """
 
         df = xr.DataArray(traces.T).to_dataset(dim='dim_0').to_dataframe()
@@ -124,6 +104,9 @@ class HoloFrame(pd.DataFrame):
         df = pd.melt(df, ('dim_1', 'dim_2'))
         df = df.rename(columns = {'dim_1':'cell', 'dim_2':'trial', 'variable':'time', 'value':'self'})
 
-        df = df.add_trialwise(df, trialwise_data, names=None)
+        hf = cls(df)
+        hf = df.add_trialwise(df, trialwise_data)
+        if frame_rate is not None:
+            hf = hf.add_secs(frame_rate=frame_rate)
 
-        return cls(df)
+        return hf
